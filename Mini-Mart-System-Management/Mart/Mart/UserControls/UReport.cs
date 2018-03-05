702e8905bb2ad05825edd8c1d690aef90f9dd47b
Thoura Lai <Thoura Lai@THOURA-PC>
2018-03-05 21:50:52 +0700

    Updated Report

---
+++ Mini-Mart-System-Management/Mart/Mart/UserControls/UReport.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Mart.DataModel;
+
+namespace Mart
+{
+    public partial class UReport : UserControl
+    {        
+        private static UReport _instance;       
+        public static UReport Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new UReport();
+                return _instance;
+            }
+        }
+        public UReport()
+        {
+            InitializeComponent();
+            this.Load += UReport_Load;
+            rdSold.CheckedChanged += rdSold_CheckedChanged;
+            rdImport.CheckedChanged += rdSold_CheckedChanged;            
+        }
+
+        void rdSold_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sender == rdSold)
+            {
+                if (rdSold.Checked)
+                {                   
+                    AddUSupSoldIntoPanel();
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        private void AddUSupSoldIntoPanel()
+        {
+            if (!pContainer.Controls.Contains(USubSold.Instance))
+            {
+                pContainer.Controls.Add(USubSold.Instance);
+                USubSold.Instance.Dock = DockStyle.Fill;
+                USubSold.Instance.BringToFront();
+            }
+            else { USubSold.Instance.BringToFront(); }
+
+            //USubSold subSold = null;
+            //if (subSold == null)
+            //{
+            //    subSold = new USubSold();
+            //}
+            //subSold.Dock = DockStyle.Fill;
+            //pContainer.Controls.Add(subSold);
+        }
+      
+        void UReport_Load(object sender, EventArgs e)
+        {
+            rdSold.Checked = true;
+            AddUSupSoldIntoPanel();
+        }
+               
+    }
+}
